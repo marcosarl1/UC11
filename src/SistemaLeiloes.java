5d598dc2e9e258d8a9d3c4336a681ba672268236
Marcos Júnior <marcosajr11@gmail.com>
2024-08-20 20:18:38 -0300

    Definindo cadastroVIEW como tela principal

---
+++ src/SistemaLeiloes.java
@@ -0,0 +1,8 @@
+import java.awt.EventQueue;
+import view.cadastroVIEW;
+
+public class SistemaLeiloes {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> new cadastroVIEW().setVisible(true));
+    }
+}
