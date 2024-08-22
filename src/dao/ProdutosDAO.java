1a8cd728ffd6cdaa2710a0648c215c2ff455e49c
Marcos Júnior <marcosajr11@gmail.com>
2024-08-21 21:24:45 -0300

    Implementado método listarProdutosVendidos()

--- src/dao/ProdutosDAO.java
+++ src/dao/ProdutosDAO.java
@@ -13,10 +13,12 @@ public class ProdutosDAO {
     Connection conn = null;
     PreparedStatement st = null;
     ResultSet rs = null;
-    private static final String CADASTRAR_PRODUTO 
+    private static final String CADASTRAR_PRODUTO
             = " INSERT INTO produtos (nome, valor, status) VALUES (?,?,?)";
     private static final String LISTAR_PRODUTOS
-            = "SELECT * FROM produtos";
+            = "SELECT * FROM produtos WHERE status = 'A Venda'";
+    private static final String LISTAR_PRODUTOS_VENDIDO_STRING
+            = "SELECT * FROM produtos WHERE status = 'Vendido'";
 
     public void cadastrarProduto(ProdutosDTO produto) throws SQLException {
         try {
@@ -40,7 +42,7 @@ public class ProdutosDAO {
             conn = new conectaDAO().connectDB();
             st = conn.prepareStatement(LISTAR_PRODUTOS);
             rs = st.executeQuery();
-            while (rs.next()) {        
+            while (rs.next()) {
                 ProdutosDTO produtosDTO = new ProdutosDTO();
                 produtosDTO.setId(rs.getInt("id"));;
                 produtosDTO.setNome(rs.getString("nome"));
@@ -54,6 +56,25 @@ public class ProdutosDAO {
             conectaDAO.closeConnection();
             conectaDAO.closeSt(st);
             conectaDAO.closeRs(rs);
+        }
+        return listaProdutos;
+    }
+
+    public List<ProdutosDTO> listarProdutosVendidos() throws SQLException {
+        List<ProdutosDTO> listaProdutos = new ArrayList<>();
+        try {
+            conn = new conectaDAO().connectDB();
+            st = conn.prepareStatement(LISTAR_PRODUTOS_VENDIDO_STRING);
+            rs = st.executeQuery();
+            while (rs.next()) {
+                ProdutosDTO produtosDTO = new ProdutosDTO();
+                produtosDTO.setId(rs.getInt("id"));
+                produtosDTO.setNome(rs.getString("nome"));
+                produtosDTO.setValor(rs.getInt("valor"));
+                produtosDTO.setStatus(rs.getString("status"));
+                listaProdutos.add(produtosDTO);
+            }
+        } catch (Exception e) {
         }
         return listaProdutos;
     }
